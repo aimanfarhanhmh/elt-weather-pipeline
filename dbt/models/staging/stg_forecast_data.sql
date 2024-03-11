501c67d443dc093380eff296dcdbc552124060e9
aimanfarhanhmh@gmail.com <aimanfarhanhmh@gmail.com>
2024-03-11 15:23:25 +0000

    init macro and staging model

--- dbt/models/staging/stg_forecast_data.sql
+++ dbt/models/staging/stg_forecast_data.sql
@@ -1,6 +1,26 @@
-{
-    { 
-        config(materialized='view') 
-    }
-}
+{{config(materialized='view')}}
+
+with source as (
+    select * from {{source('staging', 'forecast')}}
+),
+
+renamed as (
+    select
+        {{dbt_utils.generate_surrogate_key(['date','location__location_id'])}} as forecastid,
+        date as datetime,
+        location__location_id as location_id,
+        location__location_name as location_name,
+        morning_forecast,
+        afternoon_forecast,
+        night_forecast,
+        summary_forecast,
+        summary_when,
+        min_temp,
+        max_temp
+    from source
+)
+
+select * from renamed
+
+
 
